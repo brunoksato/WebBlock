61e9002f18188ecbc3962fdba4d5677987c2d4fb
Bruno Sato <bruno.sato@live.com>
2014-01-12 19:21:24 -0200

    edit angular

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,27 @@
-(function(){
+//(function(){
 
-function Load($scope, $http) {
-    $http.get({method: 'GET', url: 'get.json'}).
-        success(function(data) {
-            $scope.greeting = data;
-    });
-}
- 
+//function Load($scope, $http) {
+//    $http.get({method: 'GET', url: 'get.json'}).
+//        success(function(data) {
+//            $scope.greeting = data;
+//    });
+//}
 
-}());
+var app = angular.module('poliedroAprova', []);
+
+app.controller('edit', function ($scope) {
+    $scope.showtooltip = false;
+    $scope.value = 'Edit me.';
+
+    $scope.hideTooltip = function () {
+        $scope.showtooltip = false;
+    }
+
+    $scope.toggleTooltip = function (e) {
+        e.stopPropagation();
+        $scope.showtooltip = !$scope.showtooltip;
+    }
+});
+
+
+//}());
