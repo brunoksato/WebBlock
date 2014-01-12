e14ee57585b47254329b8edd96b75cc1c4f76542
Bruno Sato <bruno.sato@live.com>
2014-01-12 20:11:05 -0200

    load json

--- js/controller.js
+++ js/controller.js
@@ -9,6 +9,12 @@
 
 var app = angular.module('poliedroAprova', []);
 
+app.controller('load', function ($scope, $http) {
+    $http.get('get.json').then(function (data) {
+        $scope.block = data;
+    })
+});
+
 app.controller('edit', function ($scope) {
     $scope.showtooltip = false;
     $scope.value = 'Edit me.';
