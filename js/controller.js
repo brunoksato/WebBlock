7f5001aa0059a6a0cf1c06b0aa9ec81e0b0c5651
Bruno Sato <bruno.sato@live.com>
2014-01-13 02:36:47 -0200

    add bind all

--- js/controller.js
+++ js/controller.js
@@ -7,29 +7,31 @@
 //    alert($scope.result);
 //}
 
-(function(){
+(function () {
 
-var app = angular.module('poliedroApp', []);
 
-app.controller('load', function ($http, $scope) {
-    $http.get('js/get.js').success(function(data) {
-        $scope.result = data;
+
+    var app = angular.module('poliedroApp', []);
+
+    app.controller('load', function ($http, $scope) {
+        $http.get('js/get.js').success(function (data) {
+            $scope.result = data;
+        });
     });
-});
 
-//app.controller('edit', function ($scope) {
-//    $scope.showtooltip = false;
-//    $scope.value = 'Edit me.';
+    //app.controller('edit', function ($scope) {
+    //    $scope.showtooltip = false;
+    //    $scope.value = 'Edit me.';
 
-//    $scope.hideTooltip = function () {
-//        $scope.showtooltip = false;
-//    }
+    //    $scope.hideTooltip = function () {
+    //        $scope.showtooltip = false;
+    //    }
 
-//    $scope.toggleTooltip = function (e) {
-//        e.stopPropagation();
-//        $scope.showtooltip = !$scope.showtooltip;
-//    }
-//});
+    //    $scope.toggleTooltip = function (e) {
+    //        e.stopPropagation();
+    //        $scope.showtooltip = !$scope.showtooltip;
+    //    }
+    //});
 
-
-}());
+    
+} ());
--- js/get.js
+++ js/get.js
@@ -34,6 +34,140 @@
 		"escola": "Unifesp",
         "curso" : "",
         "ano"   : "2013"
+	},
+    "block6":{
+		"number": "437",
+		"escola": "Unifesp",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+     "block7":{
+		"nome": "Guilherme dos Santos Perez",
+		"list1": "1º lugar Medicina USP Pinheiro",
+        "list2" : "Medicina Unicamp, Unifesp"
+	},
+    "block8":{
+		"number": "50",
+		"escola": "ITA",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+    "block9":{
+		"number": "96",
+		"escola": "Unesp",
+        "curso" : "Engenharia",
+        "ano"   : "2013"
+	},
+    "block10":{
+		"number": "56",
+		"escola": "IME",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+    "block11":{
+		"url": "www.youtube.com/embed/f1qCXynE9I0"
+	},
+    "block12":{
+		"number": "30",
+		"escola": "Unesp",
+        "curso" : "Medicina",
+        "ano"   : "2013"
+	},
+    "block13":{
+		"number": "74",
+		"escola": "Poli-USP",
+        "curso" : "Engenharia",
+        "ano"   : "2013"
+	},
+    "block14":{
+		"number": "90",
+		"escola": "AFA",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+    "block15":{
+		"texto": "Acesse aqui os resultados das Unidades Sede do Poliedro"
+	},
+    "block16":{
+		"number": "236",
+		"escola": "Unicamp",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+    "block17":{
+		"number": "8",
+		"escola": "USP",
+        "curso" : "Direito - Riberão Preto",
+        "ano"   : "2013"
+	},
+    "block18":{
+		"nome": "Maira Fassoni Ribeiro",
+		"list1": "1º lugar Medicina USP Riberão",
+        "list2" : "Medicina Famema, Unesp, Unicamp, Unifesp"
+	},
+     "block19":{
+		"number": "12",
+		"escola": "USP",
+        "curso" : "Engenharia",
+        "ano"   : "2013"
+	},
+     "block20":{
+		"number": "251",
+		"escola": "PUC",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+     "block21":{
+		"number": "11",
+		"escola": "UERJ",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+     "block22":{
+		"number": "21",
+		"escola": "USP",
+        "curso" : "Direito - São Francisco",
+        "ano"   : "2013"
+	},
+     "block23":{
+		"number": "57",
+		"escola": "UFSC",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+     "block24":{
+		"number": "40",
+		"escola": "UFMG",
+        "curso" : "",
+        "ano"   : "2013"
+	},
+    "block25":{
+		"nome": "Rodrigo Bolini de Oliveira Lima",
+		"list1": "1º lugar Medicina Unesp",
+        "list2" : "Medicina Famema, USP, UFSCar, Unifesp, Uninio"
+	},
+     "block26":{
+		"titulo": "VOCÊ FOI APROVADO?",
+		"texto": "Clique aqui para registrar sua aprovação em nossa base de dados."
+	},
+     "block27":{
+		"number": "102",
+		"escola": "Unicamp",
+        "curso" : "Engenharia",
+        "ano"   : "2013"
+	},
+     "block28":{
+		"number": "40",
+		"escola": "UFsCar",
+        "curso" : "Engenharia",
+        "ano"   : "2013"
+	},
+     "block29":{
+		"number": "10.920",
+		"texto": "Aprovações na Unidades Parceiras do Sistema de Ensino Poliedro"
+	},
+    "block30":{
+		"url": "//www.youtube.com/embed/oqn10mlP5SM"
 	}
 }
 
