5f8f127e0f6f5da4dedcd6b279d533b5be264b40
Bruno Sato <bruno.sato@live.com>
2014-01-12 22:53:31 -0200

    add video and normal

--- js/get.js
+++ js/get.js
@@ -12,20 +12,27 @@
         "ano"   : "2013"
 	},
     "block2":{
+		"number": "35",
+		"escola": "Fanema",
+        "curso" : "Medicina",
+        "ano"   : "2013"
+	},
+    "block3":{
 		"number": "58",
 		"escola": "Unicamp",
         "curso" : "Medicina",
         "ano"   : "2013"
 	},
-    "block3":{
+    "block4":{
 		"number": "54",
 		"escola": "Santa Casa",
         "curso" : "Medicina",
         "ano"   : "2013"
 	},
-    "block4":{
+    "block5":{
 		"number": "437",
 		"escola": "Unifesp",
+        "curso" : "",
         "ano"   : "2013"
 	}
 }
